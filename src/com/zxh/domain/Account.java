9657e053e0a73dc11a230d927f6d6638d1d10449
ZhangL <2571340693@qq.com>
2021-04-04 13:03:19 +0800

    account

--- src/com/zxh/domain/Account.java
+++ src/com/zxh/domain/Account.java
@@ -3,4 +3,29 @@ package com.zxh.domain;
 public class Account {
     private int id;
 
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
