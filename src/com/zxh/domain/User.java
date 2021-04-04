a2c2bf41e91a394633da28b5c0328710ebd46654
ZhangL <2571340693@qq.com>
2021-04-04 13:18:59 +0800

    tostring

--- src/com/zxh/domain/User.java
+++ src/com/zxh/domain/User.java
@@ -21,4 +21,13 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
